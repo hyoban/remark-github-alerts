a1e024ebde7338d7655c8702a94778f2a4dc8003
Stephen Zhou <hi@hyoban.cc>
2023-12-01 17:18:11 +0800

    docs: add nextra example

---
+++ examples/nextra-template/theme.config.tsx
@@ -0,0 +1,11 @@
+import { DocsThemeConfig } from "nextra-theme-docs"
+import React from "react"
+
+const config: DocsThemeConfig = {
+  logo: <span>remark-github-alerts</span>,
+  project: {
+    link: "https://github.com/hyoban/remark-github-alerts",
+  },
+}
+
+export default config
